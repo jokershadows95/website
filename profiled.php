d39242281fda20daceb9074ef39560ed8c6c3384
jokershadows95 <faljoker.df@gmail.com>
2018-12-29 00:30:06 +0100

    Adding new info for administration
    Dodanie nowej opcji dla administracji w postaci tabeli z informacjami 
    kto chce dostac awans/podwyżkę i kiedy została owa prośba wysłana
    
    Co-Authored-By: <jokershadows95@users.noreply.github.com>

--- profiled.php
+++ profiled.php
@@ -244,6 +244,46 @@ else {
     </div>';
     echo '</div></div></div> <br/>';
     }
+		echo '
+		<div class="jumbotron jumbotron-fluid">
+			<div class="container">
+				<h1 class="display-4">Fluid jumbotron</h1>
+				<table class="table">
+				  <thead class="thead-dark">
+						    <tr>
+						      <th scope="col">#</th>
+						      <th scope="col">Name</th>
+						      <th scope="col">Surname</th>
+						      <th scope="col">Date</th>
+									<th scope="col">Awans</th>
+									<th scope="col">Opinion</th>
+						    </tr>
+						  </thead>
+						  <tbody>
+						    <tr>
+						      <th scope="row">1</th>
+						      <td>Mark</td>
+						      <td>Otto</td>
+						      <td>@mdo</td>
+						    </tr>
+						    <tr>
+						      <th scope="row">2</th>
+						      <td>Jacob</td>
+						      <td>Thornton</td>
+						      <td>@fat</td>
+						    </tr>
+						    <tr>
+						      <th scope="row">3</th>
+						      <td>Larry</td>
+						      <td>the Bird</td>
+						      <td>@twitter</td>
+						    </tr>
+				  </tbody>
+				</table>
+			</div>
+		</div>
+
+		';
     if ($profile['ranga']=="Okres próbny" || $profile['ranga']=="Kelner" || $profile['ranga']=="Barman") {
 		/*	echo '
 			<div class="container-fluid">
